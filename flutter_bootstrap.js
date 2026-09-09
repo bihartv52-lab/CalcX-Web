1bb1de611eafd7065e12f7487e4f73bd48488537
bihartv52-lab <bihartv52-lab@users.noreply.github.com>
2026-09-10 00:51:24 +0530

    Deploy latest web release with emergency panic calculator button, clickable link sharing, hover copy button, Instagram chat style, watch party, and group cleanup

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624aa1","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d937","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1965824288" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2697624973" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
